797e84176bed16aba8b5c8564ef99d0a9a1151d7
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2023-03-04 17:03:08 +0100

    feat: added logic for switching to thank you view

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 (() => {
 	"use strict";
+	const submitButton = document.querySelector("button");
+	const ratingView = document.querySelector("div.rating");
+	const thankYouView = document.querySelector("div.thank-you");
+	submitButton.addEventListener("click", function goToThankYou() {
+		ratingView.classList.toggle("display-none");
+		thankYouView.classList.toggle("display-none");
+	});
 })();
