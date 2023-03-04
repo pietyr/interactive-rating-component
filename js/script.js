929e228fd4f18cacf38101024fa2c6c29cb16c68
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2023-03-04 17:23:04 +0100

    feat: added basic validation

--- js/script.js
+++ js/script.js
@@ -4,6 +4,10 @@
 	const ratingView = document.querySelector("div.rating");
 	const thankYouView = document.querySelector("div.thank-you");
 	const output = document.querySelector("output");
+	const errorP = document.createElement("p");
+	errorP.classList.add("error");
+	errorP.innerHTML = "You have to select rating";
+	let errorShown = false;
 	submitButton.addEventListener("click", function goToThankYou() {
 		const checkedButton = document.querySelector("input:checked");
 		if (checkedButton != null) {
@@ -11,6 +15,17 @@
 			output.innerHTML = score;
 			ratingView.classList.toggle("display-none");
 			thankYouView.classList.toggle("display-none");
+		} else {
+			ratingView.appendChild(errorP);
+			errorShown = true;
 		}
 	});
+	document
+		.querySelector("ul")
+		.addEventListener("click", function hideError(e) {
+			if (errorShown && e.target.nodeName == "LABEL") {
+				ratingView.removeChild(errorP);
+				errorShown = false;
+			}
+		});
 })();
