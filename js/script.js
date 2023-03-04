ff221ff46c64be10852f19b89e252649d6fd166b
Piotr Kisiel <piotr.kisiel.6@gmail.com>
2023-03-04 17:07:44 +0100

    feat: thank you page now displays selected rating

--- js/script.js
+++ js/script.js
@@ -3,8 +3,14 @@
 	const submitButton = document.querySelector("button");
 	const ratingView = document.querySelector("div.rating");
 	const thankYouView = document.querySelector("div.thank-you");
+	const output = document.querySelector("output");
 	submitButton.addEventListener("click", function goToThankYou() {
-		ratingView.classList.toggle("display-none");
-		thankYouView.classList.toggle("display-none");
+		const checkedButton = document.querySelector("input:checked");
+		if (checkedButton != null) {
+			const score = checkedButton.getAttribute("value");
+			output.innerHTML = score;
+			ratingView.classList.toggle("display-none");
+			thankYouView.classList.toggle("display-none");
+		}
 	});
 })();
